cdfdc2e242b6d030a89576487830f7ee4649a154
lkalinka <l.kalinka>
2015-05-29 10:43:02 +0200

    small refactor for uri -> toString use in methods, add more coverage

--- Tests/RabbitMqPersistenceHandlerTest.php
+++ Tests/RabbitMqPersistenceHandlerTest.php
@@ -3,55 +3,55 @@
 namespace Simgroep\ConcurrentSpiderBundle\Tests;
 
 use PHPUnit_Framework_TestCase;
+use Simgroep\ConcurrentSpiderBundle\RabbitMqPersistenceHandler;
 
 class RabbitMqPersistenceHandlerTest extends PHPUnit_Framework_TestCase
 {
     public function testIfCanHandleApplicationPdfContentType()
     {
         $queue = $this
-            ->getMockBuilder('Simgroep\ConcurrentSpiderBundle\Queue')
-            ->disableOriginalConstructor()
-            ->setMethods(array('publish', '__destruct'))
-            ->getMock();
+                ->getMockBuilder('Simgroep\ConcurrentSpiderBundle\Queue')
+                ->disableOriginalConstructor()
+                ->setMethods(['publish', '__destruct'])
+                ->getMock();
 
         $pdfParser = $this
-            ->getMockBuilder('Smalot\PdfParser\Parser')
-            ->disableOriginalConstructor()
-            ->setMethods(array())
-            ->getMock();
+                ->getMockBuilder('Smalot\PdfParser\Parser')
+                ->disableOriginalConstructor()
+                ->getMock();
 
         $persistenceHandler = $this
-            ->getMockBuilder('Simgroep\ConcurrentSpiderBundle\RabbitMqPersistenceHandler')
-            ->setConstructorArgs(array($queue, $pdfParser))
-            ->setMethods(array('getDataFromPdfFile'))
-            ->getMock();
+                ->getMockBuilder('Simgroep\ConcurrentSpiderBundle\RabbitMqPersistenceHandler')
+                ->setConstructorArgs([$queue, $pdfParser])
+                ->setMethods(['getDataFromPdfFile'])
+                ->getMock();
 
         $persistenceHandler
-            ->expects($this->once())
-            ->method('getDataFromPdfFile')
-            ->will($this->returnValue(array()));
+                ->expects($this->once())
+                ->method('getDataFromPdfFile')
+                ->will($this->returnValue([]));
 
         $response = $this
-            ->getMockBuilder('Guzzle\Http\Message\Response')
-            ->disableOriginalConstructor()
-            ->setMethods(array('getContentType'))
-            ->getMock();
+                ->getMockBuilder('Guzzle\Http\Message\Response')
+                ->disableOriginalConstructor()
+                ->setMethods(['getContentType'])
+                ->getMock();
 
         $response
-            ->expects($this->once())
-            ->method('getContentType')
-            ->will($this->returnValue('application/pdf'));
+                ->expects($this->once())
+                ->method('getContentType')
+                ->will($this->returnValue('application/pdf'));
 
         $resource = $this
-            ->getMockBuilder('\VDB\Spider\Resource')
-            ->disableOriginalConstructor()
-            ->setMethods(array('getResponse'))
-            ->getMock();
+                ->getMockBuilder('\VDB\Spider\Resource')
+                ->disableOriginalConstructor()
+                ->setMethods(['getResponse'])
+                ->getMock();
 
         $resource
-            ->expects($this->once())
-            ->method('getResponse')
-            ->will($this->returnValue($response));
+                ->expects($this->once())
+                ->method('getResponse')
+                ->will($this->returnValue($response));
 
         $persistenceHandler->persist($resource);
     }
@@ -59,50 +59,189 @@ class RabbitMqPersistenceHandlerTest extends PHPUnit_Framework_TestCase
     public function testIfCanHandleTextHtmlContentType()
     {
         $queue = $this
-            ->getMockBuilder('Simgroep\ConcurrentSpiderBundle\Queue')
-            ->disableOriginalConstructor()
-            ->setMethods(array('publish', '__destruct'))
-            ->getMock();
+                ->getMockBuilder('Simgroep\ConcurrentSpiderBundle\Queue')
+                ->disableOriginalConstructor()
+                ->setMethods(['publish', '__destruct'])
+                ->getMock();
 
         $pdfParser = $this
-            ->getMockBuilder('Smalot\PdfParser\Parser')
-            ->disableOriginalConstructor()
-            ->setMethods(array())
-            ->getMock();
+                ->getMockBuilder('Smalot\PdfParser\Parser')
+                ->disableOriginalConstructor()
+                ->getMock();
 
         $persistenceHandler = $this
-            ->getMockBuilder('Simgroep\ConcurrentSpiderBundle\RabbitMqPersistenceHandler')
-            ->setConstructorArgs(array($queue, $pdfParser))
-            ->setMethods(array('getDataFromWebPage'))
-            ->getMock();
+                ->getMockBuilder('Simgroep\ConcurrentSpiderBundle\RabbitMqPersistenceHandler')
+                ->setConstructorArgs([$queue, $pdfParser])
+                ->setMethods(['getDataFromWebPage'])
+                ->getMock();
 
         $persistenceHandler
-            ->expects($this->once())
-            ->method('getDataFromWebPage')
-            ->will($this->returnValue(array()));
+                ->expects($this->once())
+                ->method('getDataFromWebPage')
+                ->will($this->returnValue([]));
 
         $response = $this
-            ->getMockBuilder('Guzzle\Http\Message\Response')
-            ->disableOriginalConstructor()
-            ->setMethods(array('getContentType'))
-            ->getMock();
+                ->getMockBuilder('Guzzle\Http\Message\Response')
+                ->disableOriginalConstructor()
+                ->setMethods(['getContentType'])
+                ->getMock();
 
         $response
-            ->expects($this->once())
-            ->method('getContentType')
-            ->will($this->returnValue('text/html'));
+                ->expects($this->once())
+                ->method('getContentType')
+                ->will($this->returnValue('text/html'));
 
         $resource = $this
-            ->getMockBuilder('\VDB\Spider\Resource')
-            ->disableOriginalConstructor()
-            ->setMethods(array('getResponse'))
-            ->getMock();
+                ->getMockBuilder('\VDB\Spider\Resource')
+                ->disableOriginalConstructor()
+                ->setMethods(['getResponse'])
+                ->getMock();
 
         $resource
-            ->expects($this->once())
-            ->method('getResponse')
-            ->will($this->returnValue($response));
+                ->expects($this->once())
+                ->method('getResponse')
+                ->will($this->returnValue($response));
 
         $persistenceHandler->persist($resource);
     }
+
+    public function testPersistRetrieveValidDataFromPdfFile()
+    {
+
+        $queue = $this->getMockBuilder('Simgroep\ConcurrentSpiderBundle\Queue')
+                ->disableOriginalConstructor()
+                ->setMethods(['__destruct', 'publish'])
+                ->getMock();
+
+        $pdfDocument = $this->getMockBuilder('Smalot\PdfParser\Document')
+                ->disableOriginalConstructor()
+                ->setMethods(['getText'])
+                ->getMock();
+        $pdfDocument->expects($this->once())
+                ->method('getText')
+                ->will($this->returnValue('dummy text from pdf file'));
+
+        $pdfParser = $this->getMockBuilder('Smalot\PdfParser\Parser')
+                ->setMethods(['parseContent'])
+                ->disableOriginalConstructor()
+                ->getMock();
+        $pdfParser->expects($this->once())
+                ->method('parseContent')
+                ->will($this->returnValue($pdfDocument));
+
+        $response = $this->getMockBuilder('Guzzle\Http\Message\Response')
+                ->disableOriginalConstructor()
+                ->setMethods(['getContentType'])
+                ->getMock();
+        $response->expects($this->once())
+                ->method('getContentType')
+                ->will($this->returnValue('application/pdf'));
+
+        $uri = $this->getMockBuilder('VDB\Uri\Uri')
+                ->disableOriginalConstructor()
+                ->setMethods(['toString'])
+                ->getMock();
+        $uri->expects($this->once())
+                ->method('toString')
+                ->will($this->returnValue('http://blabdummy.de/dummydir/dummyfile.pdf'));
+
+        $resource = $this->getMockBuilder('VDB\Spider\Resource')
+                ->disableOriginalConstructor()
+                ->setMethods(['getResponse', 'getUri'])
+                ->getMock();
+        $resource->expects($this->exactly(2))
+                ->method('getResponse')
+                ->will($this->returnValue($response));
+        $resource->expects($this->once())
+                ->method('getUri')
+                ->will($this->returnValue($uri));
+
+        $persistanceHandler = new RabbitMqPersistenceHandler($queue, $pdfParser);
+
+        $this->assertNull($persistanceHandler->persist($resource));
+    }
+
+    public function testPersistRetrieveValidDataFromWebPage()
+    {
+
+        $queue = $this->getMockBuilder('Simgroep\ConcurrentSpiderBundle\Queue')
+                ->disableOriginalConstructor()
+                ->setMethods(['__destruct', 'publish'])
+                ->getMock();
+
+        $pdfParser = $this->getMockBuilder('Smalot\PdfParser\Parser')
+                ->disableOriginalConstructor()
+                ->getMock();
+
+        $response = $this->getMockBuilder('Guzzle\Http\Message\Response')
+                ->disableOriginalConstructor()
+                ->setMethods(['getContentType'])
+                ->getMock();
+        $response->expects($this->any())
+                ->method('getContentType')
+                ->will($this->returnValue('text/html'));
+
+        $domNode = $this->getMockBuilder('\DOMNode')
+                ->setMethods(['text', 'each'])
+                ->getMock();
+        $domNode->expects($this->once())
+                ->method('text')
+                ->will($this->returnValue('dummy text from web page'));
+
+        $domCrawler = $this->getMockBuilder('Symfony\Component\DomCrawler\Crawler')
+                ->disableOriginalConstructor()
+                ->setMethods(['filterXpath'])
+                ->getMock();
+        $domCrawler->expects($this->at(0))
+                ->method('filterXpath')
+                ->with($this->equalTo('//title'))
+                ->will($this->returnValue($domNode));
+        $domCrawler->expects($this->at(1))
+                ->method('filterXpath')
+                ->with($this->equalTo('//*[not(self::script)]/text()'))
+                ->will($this->returnValue($domNode));
+
+        $uri = $this->getMockBuilder('VDB\Uri\Uri')
+                ->disableOriginalConstructor()
+                ->setMethods(['toString'])
+                ->getMock();
+        $uri->expects($this->any())
+                ->method('toString')
+                ->will($this->returnValue('http://blabdummy.de/dummydir/dummyfile.pdf'));
+
+        $resource = $this->getMockBuilder('VDB\Spider\Resource')
+                ->disableOriginalConstructor()
+                ->setMethods(['getResponse', 'getUri', 'getCrawler'])
+                ->getMock();
+        $resource->expects($this->once())
+                ->method('getResponse')
+                ->will($this->returnValue($response));
+        $resource->expects($this->exactly(2))
+                ->method('getCrawler')
+                ->will($this->returnValue($domCrawler));
+        $resource->expects($this->any())
+                ->method('getUri')
+                ->will($this->returnValue($uri));
+
+        $persistanceHandler = new RabbitMqPersistenceHandler($queue, $pdfParser);
+
+        $this->assertNull($persistanceHandler->persist($resource));
+    }
+
+    public function testSpiderId()
+    {
+        $queue = $this->getMockBuilder('Simgroep\ConcurrentSpiderBundle\Queue')
+                ->disableOriginalConstructor()
+                ->setMethods(['__destruct', 'publish'])
+                ->getMock();
+
+        $pdfParser = $this->getMockBuilder('Smalot\PdfParser\Parser')
+                ->disableOriginalConstructor()
+                ->getMock();
+
+        $persistanceHandler = new RabbitMqPersistenceHandler($queue, $pdfParser);
+
+        $this->assertNull($persistanceHandler->setSpiderId(123));
+    }
+
 }
