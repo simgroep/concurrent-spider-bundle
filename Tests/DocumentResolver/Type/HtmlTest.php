0840af2df8fb353dd0c4a5d75080a6e671a59dd6
lkalinka <l.kalinka>
2015-08-25 15:30:36 +0200

    added mock documents, added phpunit tests for documentResolver

---
+++ Tests/DocumentResolver/Type/HtmlTest.php
@@ -0,0 +1,298 @@
+<?php
+
+namespace Simgroep\ConcurrentSpiderBundle\DocumentResolver\Type;
+
+use PHPUnit_Framework_TestCase;
+use Simgroep\ConcurrentSpiderBundle\DocumentResolver\Type\Html;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Mock version of json_encode
+ *
+ * Used for testing an error situation.
+ *
+ * @param mixed $value
+ * @return bool|string
+ */
+function json_encode($value)
+{
+    if ($value === 'return-false') {
+        return false;
+    }
+    return \json_encode($value);
+}
+
+/**
+ * Date function mock for returning the same date string
+ * Fixing problem with generating not the same datetime each call
+ *
+ * @return string
+ */
+function date()
+{
+    return '2015-06-18T23:49:41Z';
+}
+
+class HtmlTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function isBlacklistFilterApplied()
+    {
+
+        $response = $this->getMockBuilder('Guzzle\Http\Message\Response')
+                ->disableOriginalConstructor()
+                ->setMethods(['getContentType', 'getLastModified'])
+                ->getMock();
+        $response->expects($this->once())
+                ->method('getContentType')
+                ->will($this->returnValue('text/html'));
+        $response->expects($this->once())
+                ->method('getLastModified')
+                ->will($this->returnValue('2015-06-18T23:49:41Z'));
+
+        $uri = $this->getMockBuilder('VDB\Uri\Uri')
+                ->disableOriginalConstructor()
+                ->setMethods(['toString'])
+                ->getMock();
+        $uri->expects($this->once())
+                ->method('toString')
+                ->will($this->returnValue('http://blabdummy.de/dummydir/somewebpagedummyfile.html'));
+
+        $crawler = new Crawler('', 'https://github.com');
+        $crawler->addContent('<html><body><div class="test">test</div><p>Dummy Text</p></body></html>');
+
+        $resource = $this
+                ->getMockBuilder('VDB\Spider\Resource')
+                ->disableOriginalConstructor()
+                ->setMethods(['getCrawler', 'getResponse', 'getUri'])
+                ->getMock();
+        $resource
+                ->expects($this->exactly(18))
+                ->method('getCrawler')
+                ->will($this->returnValue($crawler));
+        $resource->expects($this->exactly(2))
+                ->method('getResponse')
+                ->will($this->returnValue($response));
+        $resource->expects($this->once())
+                ->method('getUri')
+                ->will($this->returnValue($uri));
+
+        $type = new Html('.test');
+        $data = $type->getData($resource);
+
+        $this->assertEquals(0, count($crawler->filter('.test')));
+        $this->assertEquals(25, count($data['document']));
+    }
+
+    /**
+     * @test
+     */
+    public function persistRetrieveValidDataFromWebPageWithAllDefaultValuesInFields()
+    {
+
+        $response = $this->getMockBuilder('Guzzle\Http\Message\Response')
+                ->disableOriginalConstructor()
+                ->setMethods(['getContentType', 'getLastModified'])
+                ->getMock();
+        $response->expects($this->once())
+                ->method('getContentType')
+                ->will($this->returnValue('text/html'));
+        $response->expects($this->once())
+                ->method('getLastModified')
+                ->will($this->returnValue('2015-06-18T23:49:41Z'));
+
+        $uri = $this->getMockBuilder('VDB\Uri\Uri')
+                ->disableOriginalConstructor()
+                ->setMethods(['toString'])
+                ->getMock();
+        $uri->expects($this->once())
+                ->method('toString')
+                ->will($this->returnValue('http://blabdummy.de/dummydir/somewebpagedummyfile.html'));
+
+        $crawler = new Crawler('', 'https://github.com');
+        $crawler->addContent('<html><body><p>This is the text value.</p></body></html>');
+
+        $resource = $this
+                ->getMockBuilder('VDB\Spider\Resource')
+                ->disableOriginalConstructor()
+                ->setMethods(['getCrawler', 'getResponse', 'getUri'])
+                ->getMock();
+        $resource
+                ->expects($this->exactly(18))
+                ->method('getCrawler')
+                ->will($this->returnValue($crawler));
+        $resource->expects($this->exactly(2))
+                ->method('getResponse')
+                ->will($this->returnValue($response));
+        $resource->expects($this->once())
+                ->method('getUri')
+                ->will($this->returnValue($uri));
+
+        $type = new Html(null);
+        $data = $type->getData($resource);
+
+        $expectedData = [
+            'document' => [
+                'id' => sha1('http://blabdummy.de/dummydir/somewebpagedummyfile.html'),
+                'url' => 'http://blabdummy.de/dummydir/somewebpagedummyfile.html',
+                'content' => 'This is the text value.',
+                'title' => '',
+                'tstamp' => date('Y-m-d\TH:i:s\Z'),
+                'type' => ["text/html","text","html"],
+                'contentLength' => 23,
+                'lastModified'=> date('Y-m-d\TH:i:s\Z'),
+                'date' => date('Y-m-d\TH:i:s\Z'),
+                'lang' => 'nl-NL',
+                'author' => '',
+                'publishedDate' => date('Y-m-d\TH:i:s\Z'),
+                'updatedDate' => date('Y-m-d\TH:i:s\Z'),
+                'strippedContent' => 'This is the text value.',
+                'collection' => ["Alles"],
+                'description' => '',
+                'keywords'=> '',
+                'SIM_archief' => 'no',
+                'SIM.simfaq' => ['no'],
+                'DCTERMS.modified' => date('Y-m-d\TH:i:s\Z'),
+                'DCTERMS.identifier'=> 'http://blabdummy.de/dummydir/somewebpagedummyfile.html',
+                'DCTERMS.title' => '',
+                'DCTERMS.available' => date('Y-m-d\TH:i:s\Z'),
+                'DCTERMS.language'=> 'nl-NL',
+                'DCTERMS.type'=> 'webpagina'
+            ],
+        ];
+
+        $this->assertEquals($expectedData, $data);
+    }
+
+    /**
+     * @test
+     */
+    public function persistRetrieveValidDataFromWebPageWithDateAvailableAndModifiedValuesSetInFields()
+    {
+
+        $response = $this->getMockBuilder('Guzzle\Http\Message\Response')
+                ->disableOriginalConstructor()
+                ->setMethods(['getContentType', 'getLastModified'])
+                ->getMock();
+        $response->expects($this->once())
+                ->method('getContentType')
+                ->will($this->returnValue('text/html'));
+        $response->expects($this->once())
+                ->method('getLastModified')
+                ->will($this->returnValue('2015-06-18T23:49:41Z'));
+
+        $uri = $this->getMockBuilder('VDB\Uri\Uri')
+                ->disableOriginalConstructor()
+                ->setMethods(['toString'])
+                ->getMock();
+        $uri->expects($this->once())
+                ->method('toString')
+                ->will($this->returnValue('http://blabdummy.de/dummydir/somewebpagedummyfile.html'));
+
+        $crawler = new Crawler('', 'https://github.com');
+        $crawler->addContent('<html><meta name="DCTERMS.available" content="2015-06-18T23:49:41Z"><meta name="DCTERMS.modified" content="2015-06-18T23:49:41Z"><body><p>This is the text value.</p></body></html>');
+
+        $resource = $this
+                ->getMockBuilder('VDB\Spider\Resource')
+                ->disableOriginalConstructor()
+                ->setMethods(['getCrawler', 'getResponse', 'getUri'])
+                ->getMock();
+        $resource
+                ->expects($this->exactly(18))
+                ->method('getCrawler')
+                ->will($this->returnValue($crawler));
+        $resource->expects($this->exactly(2))
+                ->method('getResponse')
+                ->will($this->returnValue($response));
+        $resource->expects($this->once())
+                ->method('getUri')
+                ->will($this->returnValue($uri));
+
+        $type = new Html(null);
+        $data = $type->getData($resource);
+
+        $expectedData = [
+            'document' => [
+                'id' => sha1('http://blabdummy.de/dummydir/somewebpagedummyfile.html'),
+                'url' => 'http://blabdummy.de/dummydir/somewebpagedummyfile.html',
+                'content' => 'This is the text value.',
+                'title' => '',
+                'tstamp' => date('Y-m-d\TH:i:s\Z'),
+                'type' => ["text/html","text","html"],
+                'contentLength' => 23,
+                'lastModified'=> date('Y-m-d\TH:i:s\Z'),
+                'date' => date('Y-m-d\TH:i:s\Z'),
+                'lang' => 'nl-NL',
+                'author' => '',
+                'publishedDate' => date('Y-m-d\TH:i:s\Z'),
+                'updatedDate' => date('Y-m-d\TH:i:s\Z'),
+                'strippedContent' => 'This is the text value.',
+                'collection' => ["Alles"],
+                'description' => '',
+                'keywords'=> '',
+                'SIM_archief' => 'no',
+                'SIM.simfaq' => ['no'],
+                'DCTERMS.modified' => date('Y-m-d\TH:i:s\Z'),
+                'DCTERMS.identifier'=> 'http://blabdummy.de/dummydir/somewebpagedummyfile.html',
+                'DCTERMS.title' => '',
+                'DCTERMS.available' => date('Y-m-d\TH:i:s\Z'),
+                'DCTERMS.language'=> 'nl-NL',
+                'DCTERMS.type'=> 'webpagina'
+            ],
+        ];
+
+        $this->assertEquals($expectedData, $data);
+    }
+
+    /**
+     * @test
+     * @expectedException \Simgroep\ConcurrentSpiderBundle\InvalidContentException
+     * @expectedExceptionMessage Webpage didn't contain enough content (minimal chars is 3)
+     */
+    public function throwExceptionOnLessThenMinimalContentLength()
+    {
+
+        $response = $this->getMockBuilder('Guzzle\Http\Message\Response')
+                ->disableOriginalConstructor()
+                ->setMethods(['getContentType', 'getLastModified'])
+                ->getMock();
+        $response->expects($this->once())
+                ->method('getContentType')
+                ->will($this->returnValue('text/html'));
+        $response->expects($this->once())
+                ->method('getLastModified')
+                ->will($this->returnValue('2015-06-18T23:49:41Z'));
+
+        $uri = $this->getMockBuilder('VDB\Uri\Uri')
+                ->disableOriginalConstructor()
+                ->setMethods(['toString'])
+                ->getMock();
+        $uri->expects($this->once())
+                ->method('toString')
+                ->will($this->returnValue('http://blabdummy.de/dummydir/somewebpagedummyfile.html'));
+
+        $crawler = new Crawler('', 'https://github.com');
+
+        $resource = $this
+                ->getMockBuilder('VDB\Spider\Resource')
+                ->disableOriginalConstructor()
+                ->setMethods(['getCrawler', 'getResponse', 'getUri'])
+                ->getMock();
+        $resource
+                ->expects($this->exactly(13))
+                ->method('getCrawler')
+                ->will($this->returnValue($crawler));
+        $resource->expects($this->exactly(2))
+                ->method('getResponse')
+                ->will($this->returnValue($response));
+        $resource->expects($this->once())
+                ->method('getUri')
+                ->will($this->returnValue($uri));
+
+        $type = new Html(null);
+        $type->getData($resource);
+    }
+
+}
---
+++ Tests/DocumentResolver/Type/MsDocTest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Simgroep\ConcurrentSpiderBundle\Tests\DocumentResolver\TypeMsDoc;
+
+use PHPUnit_Framework_TestCase;
+use Simgroep\ConcurrentSpiderBundle\DocumentResolver\Type\MsDoc;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Mock version of json_encode
+ *
+ * Used for testing an error situation.
+ *
+ * @param mixed $value
+ * @return bool|string
+ */
+function json_encode($value)
+{
+    if ($value === 'return-false') {
+        return false;
+    }
+    return \json_encode($value);
+}
+
+/**
+ * Date function mock for returning the same date string
+ * Fixing problem with generating not the same datetime each call
+ *
+ * @return string
+ */
+function date()
+{
+    return '2015-06-18T23:49:41Z';
+}
+
+class MsDocTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     * @expectedException \Simgroep\ConcurrentSpiderBundle\InvalidContentException
+     * @expectedExceptionMessage MsDoc didn't contain enough content (minimal chars is 3)
+     */
+    public function throwExceptionOnLessThenMinimalContentLength()
+    {
+        $uri = $this->getMockBuilder('VDB\Uri\Uri')
+                ->disableOriginalConstructor()
+                ->setMethods(['toString'])
+                ->getMock();
+        $uri->expects($this->once())
+                ->method('toString')
+                ->will($this->returnValue('http://blabdummy.de/dummydir/sample.doc'));
+
+        $resource = $this
+                ->getMockBuilder('VDB\Spider\Resource')
+                ->disableOriginalConstructor()
+                ->setMethods(['getCrawler', 'getUri'])
+                ->getMock();
+        $resource->expects($this->once())
+                ->method('getUri')
+                ->will($this->returnValue($uri));
+
+        $type = $this->getMockBuilder('Simgroep\ConcurrentSpiderBundle\DocumentResolver\Type\MsDoc')
+                ->setMethods(['extractContentFromResource'])
+                ->getMock();
+        $type->expects($this->once())
+                ->method('extractContentFromResource')
+                ->with($resource)
+                ->will($this->returnValue(''));
+
+        $type->getData($resource);
+    }
+
+    /**
+     * @test
+     */
+    public function retrieveValidDataFromPdfFile()
+    {
+        $response = $this->getMockBuilder('Guzzle\Http\Message\Response')
+                ->disableOriginalConstructor()
+                ->setMethods(['getBody', 'getLastModified'])
+                ->getMock();
+        $response->expects($this->once())
+                ->method('getLastModified')
+                ->will($this->returnValue('2015-06-18T23:49:41Z'));
+        $response->expects($this->once())
+                ->method('getBody')
+                ->will($this->returnValue(file_get_contents(__DIR__ . '/../../Mock/Documents/sample.doc')));
+
+        $uri = $this->getMockBuilder('VDB\Uri\Uri')
+                ->disableOriginalConstructor()
+                ->setMethods(['toString'])
+                ->getMock();
+        $uri->expects($this->once())
+                ->method('toString')
+                ->will($this->returnValue('http://blabdummy.de/dummydir/sample.doc'));
+
+        $crawler = new Crawler('', 'http://blabdummy.de/dummydir/sample.doc');
+
+        $resource = $this
+                ->getMockBuilder('VDB\Spider\Resource')
+                ->disableOriginalConstructor()
+                ->setMethods(['getCrawler', 'getResponse', 'getUri', 'getBody'])
+                ->getMock();
+        $resource
+                ->expects($this->exactly(2))
+                ->method('getCrawler')
+                ->will($this->returnValue($crawler));
+        $resource->expects($this->exactly(2))
+                ->method('getResponse')
+                ->will($this->returnValue($response));
+        $resource->expects($this->once())
+                ->method('getUri')
+                ->will($this->returnValue($uri));
+
+        $type = new MsDoc();
+        $data = $type->getData($resource);
+
+        $this->assertEquals(11, count($data['document']));
+        $expectedKeys = ['id', 'url', 'content', 'title', 'tstamp', 'contentLength', 'lastModified', 'date', 'publishedDate', 'SIM_archief', 'SIM.simfaq'];
+        foreach ($expectedKeys as $expectedKey) {
+            $this->assertArrayHasKey($expectedKey, $data['document']);
+        }
+
+        $this->assertEquals('sample.doc', $data['document']['title']);
+        $this->assertNotEmpty($data, $data['document']['content']);
+    }
+
+}
