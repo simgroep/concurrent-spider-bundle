2ddeee00de3c00a191459ae6bc56696eae76ad86
Kees Schepers <kees@keesschepers.nl>
2015-09-14 12:57:39 +0200

    Fixed docblocks

--- Event/PersistenceEvents.php
+++ Event/PersistenceEvents.php
@@ -4,5 +4,11 @@ namespace Simgroep\ConcurrentSpiderBundle\Event;
 
 final class PersistenceEvents
 {
+    /**
+     * Event that is fired after data has been pulled from the resource
+     * and before this data is being send to the persistence layer.
+     *
+     * By modifying the document object in this event you can influence the data data is eventually saved.
+     */
     const PRE_PERSIST = 'simgroep_concurrent_spider.pre_persist';
 }
