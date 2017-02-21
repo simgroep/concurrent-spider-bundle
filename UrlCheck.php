f765c1dbceb22b233ed130b4904f0e026cf5afcc
Tomasz Szwed <tomasz.szwed@euvic.pl>
2017-02-21 14:23:33 +0100

    SEARCH-678 [Releasebug 2016.06] URL's with a / and without a / should only be shown once in the results

--- UrlCheck.php
+++ UrlCheck.php
@@ -108,6 +108,11 @@ class UrlCheck
     public static function fixUrl ($url) {
         $url = str_replace(' ', '%20', $url);
         $url = preg_replace('#\/\??$#i', "", $url);
+
+        if (!strpos($url, '?') && !strpos($url, '#') && !preg_match('#\.[a-z0-9]{2,5}$#i', $url)) {
+            $url = rtrim($url, '/') . '/';
+        }
+
         return $url;
     }
 }
